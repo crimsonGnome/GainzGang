46a662c97ada19fadd0a7fbc135a58b6ac06fd73
Joseph Eggers <eggersjoseph23@gmail.com>
2021-04-27 11:47:20 -0700

    adding port files

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,6 @@
 export const endpoint = `http://localhost:4444`;
 export const perPage = 6;
 export const publishableKey = `pk_test_yTIgTg38BkrGl9Rl9zT5dr5g`;
+export const prodEndpoint = `https://gainz-gang-yoga-prod.herokuapp.com/`;
 //need to at prodEndpoint
-export const portPage = 40;
+export const portPage = 6;
