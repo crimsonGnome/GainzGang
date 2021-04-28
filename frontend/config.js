0f53fe5f1871f8365b8e6d3c85993e44830e15da
Joseph Eggers <eggersjoseph23@gmail.com>
2021-04-27 21:54:18 -0700

    adding port to npm start

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,7 @@
 export const endpoint = `http://localhost:4444`;
 export const perPage = 6;
 export const publishableKey = `pk_test_yTIgTg38BkrGl9Rl9zT5dr5g`;
+export const publishableKeyLive = `pk_live_5HVHAJHXqMo6kgwxLwupv0RN00PYLlIWQL`;
 export const prodEndpoint = `https://gainz-gang-yoga-prod.herokuapp.com/`;
 //need to at prodEndpoint
 export const portPage = 6;
