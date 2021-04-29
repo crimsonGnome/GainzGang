846f1cf555d805f03fe3b4af4fa27fc143bd2f0f
Joseph Eggers <eggersjoseph23@gmail.com>
2021-04-28 21:54:38 -0700

    undoing last change (changing Coors)

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ server.express.use(async (req, res, next) => {
 server.start(
   {
     cors: {
-      credentials: false,
+      credentials: true,
       origin: process.env.FRONTEND_URL,
     },
   },
