3cf723f43ee3497ba693c2033cd2f539a23c2dd7
Joseph Eggers <eggersjoseph23@gmail.com>
2021-04-28 19:38:37 -0700

    experimenting 3.3.2

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -94,7 +94,7 @@ const Mutations = {
     ctx.response.cookie('token', token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365, //1 year cookie
-      SameSite: 'none, Secure',
+      SameSite: 'none, secure',
       Secure: true,
     });
     //Finally return the user to the browser
@@ -121,7 +121,7 @@ const Mutations = {
     ctx.response.cookie('token', token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365, //1 year cookie
-      SameSite: 'none, Secure',
+      SameSite: 'none, secure',
       Secure: true,
     });
     //Return the user
@@ -194,7 +194,7 @@ const Mutations = {
     ctx.response.cookie('token', token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365,
-      SameSite: 'none, Secure',
+      SameSite: 'none, secure',
       Secure: true,
     });
     //rerurn new user
